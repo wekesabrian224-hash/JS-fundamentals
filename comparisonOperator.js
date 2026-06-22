8342b68bb2f6810b2ae82a92643a66404f3601ba
wekesabrian224-hash <wekesabrian224@gmail.com>
2026-06-22 09:54:46 +0300

    more of the comrariosnOperation

--- comparisonOperator.js
+++ comparisonOperator.js
@@ -18,3 +18,29 @@ console.log(`numb1=${numb1}===numb2=${numb2}
     Ans ${strictEqual} its type is ${typeof strictEqual}`);
 
 let nonStrictEquality = numb1 == numb2;
+
+
+let numb3 = 20;
+let numb4 = 10;
+
+let str1 = "a";
+let str2 = "b";
+
+console.log(`num3=${numb3} and numb4=${numb4}`);
+console.log(`str1=${str1} and str2=${str2}`);
+
+// Greater than
+console.log(`numb3>numb4 ${numb3 > numb4}`);
+console.log(`str2>str1 ${str1 > str2}`);
+
+// Greater than or equal to
+console.log(`numb3>=numb4 ${numb3 >= numb4}`);
+console.log(`str2>=str1 ${str1 >= str2}`);
+
+// Less than
+console.log(`numb3<numb4 ${numb4 > numb3}`);
+console.log(`str2<str1 ${str1 < str2}`);
+
+// Less than or equal to
+console.log(`numb3<=numb4 ${numb3 <= numb4}`);
+console.log(`str2<=str1 ${str1 <= str2}`);
