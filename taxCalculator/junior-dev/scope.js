b6a375912f4289fbf6f38d179748c30982ccc04d
wekesabrian224-hash <wekesabrian224@gmail.com>
2026-07-01 07:32:42 +0300

    learnt abt the global and local scope

---
+++ taxCalculator/junior-dev/scope.js
@@ -0,0 +1,14 @@
+let name="samsung"
+function sayMyName(){
+  console.log(`name; ${name} line:3`)
+  name="iphone"
+  console.log(`${name} line :4`)
+  if(true){
+    name="tamara"
+    console.log(`name; ${name} line; 1`);
+  }
+  console.log (`${name}`)
+  let name = "Sharon"
+  let name = "lucky"
+}
+sayMyName()
