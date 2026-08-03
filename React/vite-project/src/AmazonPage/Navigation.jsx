757ca89c72557610ffd21be10ce42b315199eebd
wekesabrian224-hash <wekesabrian224@gmail.com>
2026-08-03 04:59:37 +0300

    started on the Navigation section

---
+++ React/vite-project/src/AmazonPage/Navigation.jsx
@@ -0,0 +1,11 @@
+import logo from "./assets/logo.png";
+
+//component naming
+function Navigate() {
+  return (
+    <div>
+      <h2>Navigation Section</h2>
+    </div>
+  );
+}
+export default Navigate;
--- React/vite-project/src/main.jsx
+++ React/vite-project/src/main.jsx
@@ -1,47 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-function MyComponent() {
-  return null;
-}
-
-function MyComponent2() {
-  return (
-    <div>
-      <h1>This is my second component</h1>
-    </div>
-  );
-}
-
-function ReactFragment() {
-  return (
-    <>
-      <div>
-        <h1>This is my second component</h1>
-      </div>
-      <div>
-        <h1>Welcome to the React session</h1>
-      </div>
-    </>
-  );
-}
-function ParentComponent() {
-  return (
-    <div>
-      <h1>This is a parent component</h1>
-      <MyComponent></MyComponent>
-      <MyComponent2></MyComponent2>
-      <ReactFragment></ReactFragment>
-    </div>
-  );
-}
+import Navigation from "./AmazonPage/Navigation";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <h1>My First React app</h1>
-    <MyComponent />
-    <MyComponent2></MyComponent2>
-    <ReactFragment></ReactFragment>
-    <ParentComponent></ParentComponent>
+    <Navigation />
   </StrictMode>,
 );
