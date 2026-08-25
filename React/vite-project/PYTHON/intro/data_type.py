efe55db80753d69bef162232ea5b8d205681f0b0
wekesabrian224-hash <wekesabrian224@gmail.com>
2026-08-25 10:15:15 +0300

    tackled the data stypes

---
+++ React/vite-project/PYTHON/intro/data_type.py
@@ -0,0 +1,20 @@
+# Example variables
+x = 10              # Integer
+y = 3.14             # Float
+z = "Hello"          # String
+a = True             # Boolean<True, False>
+b = [1, 2, 3]        # List
+c = {1, 2, 3}        # Set
+d = (1, 2, 3)        # Tuple
+e = {"key": "value"} # Dictionary <object>
+ # for dict we use the bracket notation
+
+ # Determining the types
+print(type(x))  # Output: <class 'int'>
+print(type(y))  # Output: <class 'float'>
+print(type(z))  # Output: <class 'str'>
+print(type(a))  # Output: <class 'bool'>
+print(type(b))  # Output: <class 'list'>
+print(type(c))  # Output: <class 'set'>
+print(type(d))  # Output: <class 'tuple'>
+print(type(e))  # Output: <class 'dict'>
---
+++ React/vite-project/PYTHON/intro/start.py
@@ -0,0 +1,10 @@
+# comments
+#console.log  →  print
+#let key  →
+
+x=34
+print("X is =",x)
+y=20
+print("Y is =",y)
+z=x+y
+print("Z is = ",z)
