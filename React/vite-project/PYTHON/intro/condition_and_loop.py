ef403e2be32a39d307a0f49c638d3eb500c92abe
wekesabrian224-hash <wekesabrian224@gmail.com>
2026-09-02 20:42:07 +0300

    learnt condition and loop and the functions

--- React/vite-project/PYTHON/intro/condition_and_loop.py
+++ React/vite-project/PYTHON/intro/condition_and_loop.py
@@ -9,11 +9,11 @@ elif age == 44:
 else:
   print("last else")  
 
-for i in range(0,200,5): 
+for i in range(0,500,50): 
   print("I is ", i)
 
 ar=[23,45,67,45,40]
-for i in range(0,len(ar)):
+for i in range(3,len(ar)):
   single_item=ar[i]
   print(single_item)
 
@@ -24,3 +24,14 @@ k=0
 while k<20:
   k=k+1
   print("k is ", k)
+
+
+# for the range we have the (start,stop ,range)===> for in range (0,30,2)
+for i in range(0,200):
+  print("I is:", i)
+
+#len gives you the length of the arrary  
+
+
+if age>80:
+  pass
--- React/vite-project/PYTHON/intro/functions.py
+++ React/vite-project/PYTHON/intro/functions.py
@@ -1,10 +1,15 @@
 def area_circle(radius):
   ans= 3.142*radius*radius
-  print(f"for circle of radius{radius}area {ans}")
+  print(f"for circle of radius{radius}area is {ans}")
   return
 
 area_circle(20)
 
 new_f=area_circle
 
-new_f(10)
+new_f(10) #calling the functions witha new name does not change the meaning as we have changed the nini tu a new name
+
+
+ #matlab ===> writing code using the matimatical stuff
+ #mostly used in making games((((python))))
+
